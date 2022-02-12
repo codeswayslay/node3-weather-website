413be63607f15569b60bb5205eff6a6346769fa9
Akinwale Agbaje <akinwaleagbaje@yahoo.com>
2022-02-12 17:21:19 +0100

    Removed unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Information for client!");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
